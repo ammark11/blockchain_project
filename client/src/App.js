ff64080cfc1a33e1a1dd91019e6f17edeaef8a37
ammark11 <ammark11cod7@gmail.com>
2023-11-16 03:03:59 +0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainHeader from "./components/Header/MainHeader";
 import Header from "./components/Header/Header";
 import Main from "./pages/main";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Lists from '../src/pages/lists'
+import Lists from '../src/pages/lists';
 import Generator from "./pages/rsa-generator";
 import BlockGenerator from "./pages/block-generate";
 import TransactionDetails from "./pages/transactionDetails";
@@ -13,21 +12,20 @@ import NodesDashboard from './NodesDashboard';
 import Dashboard from './pages/Dashboard'; 
 import UpdateTransaction from './UpdateTransaction';
 
-
 function App() {
   return (
     <BrowserRouter>
       <div className="main">
-        <Routes >
+        <Routes>
           <Route path="/register-node" element={<NodeRegistration />} />
           <Route path="/nodes-dashboard" element={<NodesDashboard />} />
-          <Route path="" element={[<MainHeader />, <Main />]}/> 
+          <Route path="/" element={[<MainHeader />, <Main />]} />
           <Route path="/lists" element={[<Header/>, <Lists />]} />
           <Route path="/generator" element={[<Header />, <Generator />]} />
           <Route path="/block-generate" element={[<Header />, <BlockGenerator />]} />
           <Route path="/transaction-detail/:index" element={[<MainHeader />, <TransactionDetails />]} />
-          <Route path="/update-transaction" component={UpdateTransaction} />
-          <Route  path="./pages/Dashboard" component={Dashboard} />
+          <Route path="/update-transaction" element={<UpdateTransaction />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
     </BrowserRouter>
